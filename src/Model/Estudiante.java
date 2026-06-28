38e349e2ed55ad5325d766fb4489d89a44334a5a
PaciosDani <mateopb837@gmail.com>
2026-06-27 21:51:25 -0600

    Estudiante

--- src/Model/Estudiante.java
+++ src/Model/Estudiante.java
@@ -1,4 +1,87 @@
 package Model;
 
+import java.time.LocalDate;
+
 public class Estudiante {
+    private int id_estudiante;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String telefono;
+    private String direccion;
+    private LocalDate fecha_nacimiento;
+    private boolean activo;
+
+    public int getId() {
+        return id_estudiante;
+    }
+
+    public void setId(int id_estudiante) {
+        this.id_estudiante = id_estudiante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
+
+    @Override
+    public String toString() {
+        return getNombreCompleto();
+    }
 }
