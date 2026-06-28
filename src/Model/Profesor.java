6fe58bd09aae64bf5ad0497785f4adfef5cb4725
PaciosDani <mateopb837@gmail.com>
2026-06-27 21:54:12 -0600

    Se crea el modelo de profesor

--- src/Model/Profesor.java
+++ src/Model/Profesor.java
@@ -1,4 +1,77 @@
 package Model;
 
 public class Profesor {
+    private int id_profesor;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String email;
+    private String telefono;
+    private String especialidad;
+    private boolean activo;
+
+    public int getId() {
+        return id_profesor;
+    }
+
+    public void setId(int id_profesor) {
+        this.id_profesor = id_profesor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
+
+    @Override
+    public String toString() {
+        return getNombreCompleto();
+    }
 }
