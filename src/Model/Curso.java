d3f66b13e1e22d5edac5dbc8e325836da9772e64
astridnavar21 <navarrojimena634@gmail.com>
2026-07-29 20:32:40 -0600

    Se creó el modelo Curso.

---
+++ src/Model/Curso.java
@@ -0,0 +1,81 @@
+package Model;
+
+public class Curso {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int creditos;
+    private int profesorId;
+    private String nombreProfesor;
+    private int cupoMaximo = 30;
+    private boolean activo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public int getProfesorId() {
+        return profesorId;
+    }
+
+    public void setProfesorId(int profesorId) {
+        this.profesorId = profesorId;
+    }
+
+    public String getNombreProfesor() {
+        return nombreProfesor;
+    }
+
+    public void setNombreProfesor(String nombreProfesor) {
+        this.nombreProfesor = nombreProfesor;
+    }
+
+    public int getCupoMaximo() {
+        return cupoMaximo;
+    }
+
+    public void setCupoMaximo(int cupoMaximo) {
+        this.cupoMaximo = cupoMaximo;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
